904f928eb6bd23670422abaaba64cfea5beab4ae
Lelle222 <161458991+Lelle222@users.noreply.github.com>
2024-06-17 12:31:18 +0200

    feature-02-implementation login

--- src/lib/common.js
+++ src/lib/common.js
@@ -34,3 +34,33 @@ function register() {
     return;
   }
 }
+function login() {
+  event.preventDefault();
+
+  var user = document.getElementById("user").value;
+  var password = document.getElementById("pass").value;
+
+  var i = userArray.indexOf(email);
+
+  if (userArray.indexOf(email) == -1) {
+    if (user == "") {
+      alert("User required.");
+      return;
+    }
+    alert("User does not exist.");
+    return;
+  } else if (passwordArray[i] != password) {
+    if (password == "") {
+      alert("Password required.");
+      return;
+    }
+    alert("Password does not match.");
+    return;
+  } else {
+    alert(user + " yor are login Now \n welcome to our website.");
+
+    document.getElementById("user").value = "";
+    document.getElementById("pass").value = "";
+    return;
+  }
+}
