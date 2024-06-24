f3965e887bad57545a15702466601690935fd975
Gianfranco Baracca <toph99beifong@gmail.com>
2024-06-24 09:38:28 +0200

    test: add more login validation tests, switch from jest to vitest

--- src/lib/login.js
+++ src/lib/login.js
@@ -5,19 +5,19 @@ const PASS_KEY = "pass";
 const CONFIRM_KEY = "confirm";
 const USERS_KEY = "users";
 //Funzione per prendere i dati
-function getCredentialRegister() {
+export function getCredentialRegister() {
   const name = document.getElementById(USERNAME_KEY).value;
   const password = document.getElementById(PASS_KEY).value;
   const passwordConfirm = document.getElementById(CONFIRM_KEY).value;
   return { name, password, passwordConfirm };
 }
-function getCredentialLogin() {
+export function getCredentialLogin() {
   const name = document.getElementById(USERNAME_KEY).value;
   const password = document.getElementById(PASS_KEY).value;
   return { name, password };
 }
 //Funzione registrazione
-function register({ name, password, passwordConfirm }) {
+export function register({ name, password, passwordConfirm }) {
   const validationErrors = validateRegister({
     name,
     password,
@@ -28,31 +28,25 @@ function register({ name, password, passwordConfirm }) {
     return;
   }
   const id = generateId();
-  const newUser = new User({ id, name });
-  alert(name + " grazie per esserti registrato. \nProva ad accedere ora.");
-
   // prendi l'oggetto users dalla key USERS_KEY, convertilo in oggetto con JSON.parse(), verifica che non sia già presente l'utente, se non è presente fai il push del nuovo utente nell'array, infine salva il nuovo array nel localstorage
   // Recupera l'oggetto users dal localStorage
-  let users = JSON.parse(localStorage.getItem(USERS_KEY)) || [];
+  const users = JSON.parse(localStorage.getItem(USERS_KEY)) || [];
+  const user = users.find((user) => user.id === newUser.id);
+  if(user) return user;
 
   // Verifica che l'utente non sia già presente
-  let userExists = users.some((user) => user.id === newUser.id);
+  users.push(new User({ id, name }));
 
-  if (!userExists) {
-    // Aggiungi il nuovo utente all'array
-    users.push(newUser);
-
-    // Salva il nuovo array nel localStorage
-    localStorage.setItem(id, JSON.stringify(users));
-    alert("Registrazione completata!");
-  }
+  // Salva il nuovo array nel localStorage
+  localStorage.setItem(id, JSON.stringify(users));
+  return !!newUser;
 }
 //Funzione calcolo id univoco
-function generateId() {
+export function generateId() {
   return Math.random() * 9999999;
 }
 //Funzione login
-function login(/*{ user, password }*/) {
+export function login(/*{ user, password }*/) {
   // Recuperare tutti gli utenti dal localStorage
   const user = getCredentialLogin({ user });
   const users = JSON.parse(localStorage.getItem(USERS_KEY));
@@ -70,28 +64,22 @@ function login(/*{ user, password }*/) {
 }
 
 //Funzione per controllare i dati in ingresso
-function validateRegister({ name, password, passwordConfirm }) {
-  const regEx =
-    "/^(?=.*[a-z])(?=.*[A-Z])(?=.*d)(?=.*[@.#$!%*?&])[A-Za-zd@.#$!%*?&]{8,15}$/";
+export function validateRegister({ name, password, passwordConfirm }) {
   let errors = {};
-  if (name.length > 10 || name.length < 3)
+
+  const regEx = new RegExp(
+    "/^(?=.*[a-z])(?=.*[A-Z])(?=.*d)(?=.*[@.#$!%*?&])[A-Za-zd@.#$!%*?&]{8,15}$/"
+  );
+
+  if ( name.length < 3 || name.length > 10 )
     errors[USERNAME_KEY] = "The username must be between 3 and 10 characters";
 
-  if (name === "") errors[PASS_KEY] = "Username required";
-  if (password === "") errors[PASS_KEY] = "Password required";
+  if (name === "") errors[USERNAME_KEY] = "Username required";
   if (passwordConfirm === "") errors[CONFIRM_KEY] = "Confirm password required";
-  if (password !== passwordConfirm)
+  if (password !== passwordConfirm && passwordConfirm !== "")
     errors[CONFIRM_KEY] = "Password don't match retype your Password";
 
   if (!regEx.test(password))
-    errors[PASS_KEY] = "La password non include caratteri speciali";
+    errors[PASS_KEY] = "Invalid password";
   return errors;
 }
-module.exports = {
-  validateRegister,
-  generateId,
-  getCredentialLogin,
-  getCredentialRegister,
-  register,
-  login,
-};
--- tests/sum.test.js
+++ tests/sum.test.js
@@ -1,4 +1,5 @@
-const { sum } = require("./sum");
+import { sum } from "./sum.js";
+import { expect, test } from "vitest";
 
 test("adds 1 + 2 to equal 3", () => {
   expect(sum(1, 2)).toBe(3);
